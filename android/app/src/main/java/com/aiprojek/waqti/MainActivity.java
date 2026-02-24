90db3732016a8301497c020ff9ecf7ad0444cd94
aiprojek <aiprojek01@gmail.com>
2026-02-24 09:45:04 +0700

    feat: add Capacitor Android and Tauri platforms to the project.

---
+++ android/app/src/main/java/com/aiprojek/waqti/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aiprojek.waqti;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aiprojek.waqti',
+  appName: 'Waqti',
+  webDir: 'dist'
+};
+
+export default config;
